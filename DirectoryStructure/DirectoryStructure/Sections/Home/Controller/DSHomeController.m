2c8240a8e2c33202b64bf291eb31931a47205248
zhxuewei <303453384@qq.com>
2016-07-22 15:41:14 +0800

    主页控制器修改

--- DirectoryStructure/DirectoryStructure/Sections/Home/Controller/DSHomeController.m
+++ DirectoryStructure/DirectoryStructure/Sections/Home/Controller/DSHomeController.m
@@ -18,27 +18,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [DSNOTIFICATION_CENTER postNotificationName:@"" object:nil];
     
    DSLog(@"%f",self.view.width);
-    
-    //NSString *imgIcon = @"";
-    //UIImage *img = [UIImage imageNamed:imgIcon];
-    //CGFloat imgWidth = img.width;
-    //CGFloat imgHeight = img.height;
-    //CGRect imgViewRect = CGRectMake(100, 100, imgWidth, imgHeight);
-    //创建一个imageview
-    UIImageView *imgView = [[UIImageView alloc] init];
-    //[imgView setFrame:imgViewRect];
-    imgView.backgroundColor = [UIColor whiteColor];
-    //[imgView setImage:[UIImage imageNamed:imgIcon]];
-    //imgView.layer.masksToBounds = YES;
-    //imgView.layer.cornerRadius = 5;
-    [self.view addSubview:imgView];
-    [imgView sd_setImageWithURL:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
-        
-    }];
-    
 }
 
 
