b092d702f53e73b9474035ed9aa4d3ffc30bf6b6
zhxuewei <303453384@qq.com>
2016-07-22 18:11:45 +0800

    iOS之应用程序国际化/内容国际化

--- DirectoryStructure/DirectoryStructure/Sections/Home/Controller/DSHomeController.m
+++ DirectoryStructure/DirectoryStructure/Sections/Home/Controller/DSHomeController.m
@@ -29,7 +29,36 @@
         DSLog(@" %@ BACK", self.view);
     });
     
-    UILabel*label;
+    
+    
+    //label的frame
+    CGRect labRect = CGRectMake(100, 100, 200, 30);
+    //创建一个label
+    UILabel*label = [[UILabel alloc] initWithFrame:labRect];
+    label.textColor = [UIColor redColor];
+    label.text = NSLocalizedString(@"key", nil);
+//    label.text = MyLocal(@"key");
+//    label.text = NSLocalizedString(@"key", @"Localizable");
+    [self.view addSubview:label];
+    
+    //label的frame
+    CGRect labRect1 = CGRectMake(100, 150, 200, 30);
+    //创建一个label
+    UILabel*label1 = [[UILabel alloc] initWithFrame:labRect1];
+    label1.textColor = [UIColor greenColor];
+//    label1.text = NSLocalizedString(@"key", nil);
+    label1.text = MyLocal(@"key");
+//    label1.text = NSLocalizedString(@"key", @"Localizable");
+    [self.view addSubview:label1];
+    //label的frame
+    CGRect labRect2 = CGRectMake(100, 200, 200, 30);
+    //创建一个label
+    UILabel*label2 = [[UILabel alloc] initWithFrame:labRect2];
+    label2.textColor = [UIColor blueColor];
+//    label2.text = NSLocalizedString(@"key", nil);
+//    label2.text = MyLocal(@"key");
+    label2.text = NSLocalizedString(@"key", @"Localizable");
+    [self.view addSubview:label2];
     NSString *str;
     if (IsNilOrNull(label)) {
         DSLog(@".label.");
@@ -37,7 +66,6 @@
     if (IsNilOrNull(str)) {
         DSLog(@".str.");
     }
-    
 }
 
 
